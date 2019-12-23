921284769b99bcfc729a87734ca8fc0e11da3efc
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2019-12-22 21:55:41 -0800

    Create BulkExport.ps1

---
+++ BulkExport.ps1
@@ -0,0 +1,54 @@
+# Step 1: Construct your API headers
+$accessToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+$orgId = "0da0a9c0-xxxx-xxxx-xxxx-05a0fe6aa5c4"
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.add("Authorization","Bearer $accessToken")
+$headers.add("Content-Type","application/json")
+
+# Step 2: Create the bulk export
+$body = @"
+{
+	"type": "organization_memberships_export"
+}
+"@
+
+$uri1 = "https://api-d.docusign.net/management/v2/organizations/$orgId/exports/user_list"
+$result1 = Invoke-WebRequest -headers $headers -Uri $uri1 -body $body -Method POST
+$result1.Content
+$results = $result1 | convertfrom-json
+$requestId = $results.id
+$resultId = "To be set"
+Write-Output "The request is being created. Waiting 5 seconds to get the request status."
+Start-Sleep -Second 5
+
+# Step 3: Check the Request Status
+Write-Output "Checking Bulk Action Status"
+$uri2 = "https://api-d.docusign.net/management/v2/organizations/$orgId/exports/user_list/$requestId"
+$result2 = Invoke-WebRequest -headers $headers -Uri $uri2 -Method GET
+$result2.Content
+$results = $result2 | convertfrom-json
+$retrycount = 0
+Do{
+	Write-Output "The Bulk Action has not been completed. Retrying in 5 seconds. To abort, Press Control+C"
+	Start-Sleep 5
+	$result2 = Invoke-WebRequest -headers $headers -Uri $uri2 -Method Get
+	$retrycount++
+	if($retrycount -eq 5){
+		break
+	}
+} While ($results.status -ne "completed")
+if($results.status -eq "completed"){
+	Write-Output $results.results.id
+	$resultId = $results.results.id
+}
+else{
+	Write-Output "An error has occurred, the Bulk Action has not been completed."
+}
+
+# Step 4: Download the exported CSV user list
+if($results.status -eq "completed"){
+	$uri3 = "https://demo.docusign.net/restapi/v2/organization_exports/$orgId/user_list/$resultId"
+	$result3 = Invoke-WebRequest -headers $headers -Uri $uri3 -Method GET
+	$result3.Content
+}
